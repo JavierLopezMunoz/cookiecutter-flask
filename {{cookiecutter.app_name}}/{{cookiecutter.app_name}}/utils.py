97c5ad15a4c3db4aafc9b989b353c3619d2f178c
Javier Lopez <javier@lopezmunoz.name>
2016-12-22 15:33:25 +0100

    Add AttrDict (dict with access as an object) to utils

--- {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/utils.py
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/utils.py
@@ -3,6 +3,13 @@
 from flask import flash
 
 
+# http://stackoverflow.com/questions/4984647/accessing-dict-keys-like-an-attribute-in-python
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+
 def flash_errors(form, category='warning'):
     """Flash all errors for a form."""
     for field, errors in form.errors.items():
