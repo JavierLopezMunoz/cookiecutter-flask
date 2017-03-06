7104b1139e41c7bb9d269965cab465ac4259f1a3
Javier Lopez <javier@lopezmunoz.name>
2017-03-06 10:57:11 +0100

    Remove unused webtest

--- {{cookiecutter.app_name}}/tests/conftest.py
+++ {{cookiecutter.app_name}}/tests/conftest.py
@@ -1,7 +1,6 @@
 """Defines fixtures available to all tests."""
 
 import pytest
-from webtest import TestApp
 
 from {{cookiecutter.app_name}}.app import create_app
 from {{cookiecutter.app_name}}.database import db as _db
@@ -20,12 +19,6 @@ def app():
     yield _app
 
     ctx.pop()
-
-
-@pytest.fixture(scope='function')
-def testapp(app):
-    """A Webtest app."""
-    return TestApp(app)
 
 
 @pytest.yield_fixture(scope='function')
